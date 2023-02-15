fc5492794b21eb77968556b8cd44a816f7a5443d
Cuauhtli Herrera <50902390+CuauhtliHC@users.noreply.github.com>
2023-02-15 14:49:16 -0600

    Revert "Feature/list package"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { Container } from '@mui/material';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Links from './routes/routes.jsx';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Container sx={{ mt: '66px', padding: 0 }}>
-        <Links />
-      </Container>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
