82399892defdc535f52c41ceca2fbdb12f44b06c
Cuauhtli <cuau_daali@hotmail.com>
2023-02-15 14:43:26 -0600

    View get package and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Container } from '@mui/material';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
 import Links from './routes/routes.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-      <Links />
+      <Navbar />
+      <Container sx={{ mt: '66px', padding: 0 }}>
+        <Links />
+      </Container>
     </BrowserRouter>
   );
 }
