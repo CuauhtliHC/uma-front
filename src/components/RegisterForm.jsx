26d5fcd6287bad1100b676c6dfac795d389580e1
Francisco <francisco.leyescampoy@gmail.com>
2023-02-16 20:40:24 -0300

    Validaciones de los formularios: validacion del mail, para que sea uno real; validacion de contraseña para que tenga una mayuscula, un numero y como minimo 8 caracteres; refactorizacion del codigo.

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormControl,
   InputLabel,
@@ -7,8 +7,47 @@ import {
   Button,
   Grid,
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const RegisterForm = ({ data, setData }) => {
+const RegisterForm = () => {
+  const [errors, setErrors] = useState({});
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const validate = (dato) => {
+    const errores = {};
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!dato.email) {
+      errores.email = 'El campo Email es obligatorio';
+    } else if (!emailRegex.test(dato.email)) {
+      errores.email = 'El campo Email no es válido';
+    }
+
+    if (!dato.password) {
+      errores.password = 'El campo Contraseña es obligatorio';
+    } else if (!passwordRegex.test(dato.password)) {
+      errores.password = 'La contraseña debe tener al menos 8 caracteres, una mayúscula y un número';
+    }
+
+    if (!dato.passwordConfirm) {
+      errores.passwordConfirm = 'Repite tu contraseña';
+    } else if (dato.password !== dato.passwordConfirm) {
+      errores.passwordConfirm = 'Las contraseñas no coinciden';
+    }
+
+    return errores;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const VerificateForm = validate(data);
+    setErrors(VerificateForm);
+    // if (Object.keys(VerificateForm).length === 0)
+  };
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -21,10 +60,13 @@ const RegisterForm = ({ data, setData }) => {
           <Input
             id="email"
             type="email"
-            aria-aria-describedby="email-helper"
             onChange={(e) => setData({ ...data, email: e.target.value })}
           />
-          <FormHelperText id="email-helper">Ingresa tu Email</FormHelperText>
+          {errors.email ? (
+            <FormHelperText error>{errors.email}</FormHelperText>
+          ) : (
+            <FormHelperText id="email-helper">Ingresa tu Email</FormHelperText>
+          )}
         </FormControl>
       </Grid>
       <Grid item xs={12}>
@@ -37,42 +79,50 @@ const RegisterForm = ({ data, setData }) => {
           <Input
             id="pwd"
             type="password"
-            aria-aria-describedby="password-helper"
             onChange={(e) => setData({ ...data, password: e.target.value })}
           />
-          <FormHelperText id="password-helper">
-            Ingresa tu Contraseña
-          </FormHelperText>
+          {errors.password ? (
+            <FormHelperText error>{errors.password}</FormHelperText>
+          ) : (
+            <FormHelperText id="password-helper">
+              Ingresa tu Contraseña
+            </FormHelperText>
+          )}
         </FormControl>
       </Grid>
       <Grid item xs={12}>
         <FormControl fullWidth={true}>
           <InputLabel
-            htmlFor="pwd"
+            htmlFor="pwd2"
             style={{ fontSize: '17px', color: '#FEBC14' }}>
             Contraseña
           </InputLabel>
           <Input
-            id="pwd"
+            id="pwd2"
             type="password"
-            aria-aria-describedby="password-helper"
             onChange={(e) => setData({ ...data, passwordConfirm: e.target.value })
             }
           />
-          <FormHelperText id="password-helper">
-            Repite tu Contraseña
-          </FormHelperText>
+          {errors.passwordConfirm ? (
+            <FormHelperText error>{errors.passwordConfirm}</FormHelperText>
+          ) : (
+            <FormHelperText id="password-helper">
+              Repite tu contraseña
+            </FormHelperText>
+          )}
         </FormControl>
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" fullWidth={true}>
+        <Button variant="contained" fullWidth={true} onClick={handleSubmit}>
           Crear Usuario
         </Button>
-        <Button
-          fullWidth={true}
-          style={{ textTransform: 'none', textAlign: 'center' }}>
-          Regresar
-        </Button>
+        <Link to={'/login'} style={{ textDecoration: 'none' }}>
+          <Button
+            fullWidth={true}
+            style={{ textTransform: 'none', textAlign: 'center' }}>
+            Regresar
+          </Button>
+        </Link>
       </Grid>
     </Grid>
   );
