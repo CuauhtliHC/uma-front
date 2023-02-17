5def0ea3ddeef37229efc71513c1f8af419ad91a
Cuauhtli <cuau_daali@hotmail.com>
2023-02-16 18:36:22 -0600

    Adding view restore password

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,6 +5,7 @@ import Workday from '../pages/Workday.jsx';
 import Login from '../pages/Login.jsx';
 import Register from '../pages/Register.jsx';
 import Profile from '../pages/Profile.jsx';
+import RestorePassword from '../pages/RestorePassword.jsx';
 
 const Links = () => {
   return (
@@ -14,6 +15,7 @@ const Links = () => {
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/register" element={<Register />} />
       <Route exact path="/mi_perfil" element={<Profile />} />
+      <Route exact path="/recuperar_contraseña" element={<RestorePassword />} />
     </Routes>
   );
 };
