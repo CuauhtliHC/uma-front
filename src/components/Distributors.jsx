faf3c7e7810ed4ea3f53e31a800fd245538aa32b
Francisco <francisco.leyescampoy@gmail.com>
2023-02-23 21:24:51 -0300

    se termino la vista de gestionar repartidores

--- src/components/Distributors.jsx
+++ src/components/Distributors.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../statics/styles/workday.css';
 import {
   Accordion,
@@ -6,12 +6,21 @@ import {
   AccordionSummary,
   Box,
   Typography,
+  Button,
+  Icon,
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import repartidoresFake from '../statics/DummyData/repartidoresFake';
 import DistributorsCard from './DistributorsCard.jsx';
+import more from '../statics/images/more.png';
 
 const Distributors = () => {
+  const [showCount, setShowCount] = useState(3);
+
+  const handleShowMore = () => {
+    setShowCount(showCount + 3);
+  };
+
   return (
     <Box sx={{ margin: '5vw' }}>
       <Accordion>
@@ -19,7 +28,7 @@ const Distributors = () => {
           <Typography fontWeight="bold">Repartidores</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          {repartidoresFake.map((data) => (
+          {repartidoresFake.slice(0, showCount).map((data) => (
             <DistributorsCard
               key={data.id}
               name={data.name}
@@ -28,6 +37,9 @@ const Distributors = () => {
               avatar={data.img}
             />
           ))}
+          {showCount < repartidoresFake.length && (
+            <img src={more} alt='more' onClick={handleShowMore} style={{ marginLeft: '50%' }} />
+          )}
         </AccordionDetails>
       </Accordion>
     </Box>
