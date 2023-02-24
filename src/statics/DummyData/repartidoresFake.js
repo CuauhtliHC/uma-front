6754890160a73bf84dbfaf10341ee4dcf0744bf2
giselaArroyo <giselaarroyogalarce@gmail.com>
2023-02-24 15:24:43 -0300

    add img avatar dealer

--- src/statics/DummyData/repartidoresFake.js
+++ src/statics/DummyData/repartidoresFake.js
@@ -6,21 +6,21 @@ const repartidoresFake = [
     name: 'Farid',
     status: 'Inactivo',
     percentage: 45,
-    img: Avatar,
+    img: 'https://www.shutterstock.com/image-photo/portrait-young-smiling-caucasian-man-260nw-1491969899.jpg',
   },
   {
     id: 2,
     name: 'Luciana',
     status: 'Viaje en curso',
     percentage: 50,
-    img: Avatar,
+    img: 'https://img.freepik.com/fotos-premium/joven-mujer-bonita-caucasica-blanco_1187-75943.jpg?w=2000',
   },
   {
     id: 3,
     name: 'Santiago',
     status: 'Finalizó',
     percentage: 100,
-    img: Avatar,
+    img: 'https://us.123rf.com/450wm/racorn/racorn1607/racorn160700146/61146943-hombre-joven-con-barba-hermoso-pensativo-mirando-pensativo-a-la-c%C3%A1mara-en-un-su%C3%A9ter-elegante-gris-y-.jpg',
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const repartidoresFake = [
     name: 'Luke',
     status: 'Finalizó',
     percentage: 100,
-    img: Avatar,
+    img: 'https://img.freepik.com/foto-gratis/retrato-hombre-blanco-aislado_53876-40306.jpg?w=2000',
   },
 ];
 
