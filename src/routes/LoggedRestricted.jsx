822386f355ea4498401181c1f9d49a682bf3a128
giselaArroyo <giselaarroyogalarce@gmail.com>
2023-04-21 11:05:47 -0300

    conectamos historial y paquetes pendientes

--- src/routes/LoggedRestricted.jsx
+++ src/routes/LoggedRestricted.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router';
+import dayjs from 'dayjs';
 import { dateFormat } from '../utils/today.jsx';
 
-const LoggedRestricted = ({
-  user,
-  redirectPath = '/ddjj',
-  children,
-}) => {
+const LoggedRestricted = ({ user, redirectPath = '/ddjj', children }) => {
+  const dateToDay = dayjs().format('YYYY-MM-DD');
   if (user) {
     if (user.isAdmin === 'ADMIN_ROL') {
       return <Navigate to={`/gestionar_agenda/${dateFormat}`} replace />;
+    } else if (user.ddjj.date === dateToDay) {
+      return <Navigate to={'/iniciar_jornada'} replace />;
     }
     return <Navigate to={redirectPath} replace />;
   }
