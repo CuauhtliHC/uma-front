be93efd129a00f536c231b7d72f560b6eefe624b
Cuauhtli <cuau_daali@hotmail.com>
2023-04-20 19:29:38 -0600

    adding details list user and shedule

--- src/utils/forms/createPackage.jsx
+++ src/utils/forms/createPackage.jsx
@@ -2,9 +2,23 @@ import axios from 'axios';
 
 const publicUrl = process.env.REACT_APP_URL_BACKEND;
 
-const functCreatePkg = (formValue) => {
+const functCreatePkg = ({
+  address,
+  addressee,
+  dateOfDelivery,
+  quantityOfPackages,
+  weight,
+}) => {
+  const quantity = parseInt(quantityOfPackages, 10);
+  const weightFloat = parseFloat(weight);
   try {
-    axios.post(`${publicUrl}package/create`, formValue);
+    axios.post(`${publicUrl}package/create`, {
+      address,
+      addresses: addressee,
+      dateOfDelivery,
+      quantityOfPackages: quantity,
+      weight: weightFloat,
+    });
   } catch (error) {
     console.log(error);
   }
