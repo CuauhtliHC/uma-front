a744089ab423a45f97c7869feb7637a4490c963a
Francisco <francisco.leyescampoy@gmail.com>
2023-04-27 18:28:05 -0300

    Se modifico un poco la vista del perfil del usuario y se añadio un navigate al logo de fast delivery que redirige al usuario a su vista principal dependiendo si es userAdmin o user.

--- src/commons/navbar/LogoNavBar.jsx
+++ src/commons/navbar/LogoNavBar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+import { useRecoilValue } from 'recoil';
 import { ImgLogoNavBar } from '../../statics/styles/navbarStyle.jsx';
 import logoFD from '../../statics/images/logoFastDelivery.jpeg';
+import { user } from '../../state/user.jsx';
+import { dateFormat } from '../../utils/today.jsx';
 
 const LogoNavBar = () => {
+  const navigate = useNavigate();
+  const dataUser = useRecoilValue(user);
   return (
     <>
-      <ImgLogoNavBar component="img" alt="Logo" src={logoFD} />
+      <ImgLogoNavBar
+        component="img"
+        alt="Logo"
+        src={logoFD}
+        onClick={() => navigate(
+          dataUser.isAdmin === 'ADMIN_ROL'
+            ? `/gestionar_agenda/${dateFormat}`
+            : '/iniciar_jornada',
+        )
+        }
+      />
     </>
   );
 };
